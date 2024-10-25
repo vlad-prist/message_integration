7ebad90d934414b2554efc3bc6aaf40b0e29396a
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-10-25 14:47:37 +0300

    app users

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from users.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f'Пользователь с почтой "{email}" уже существует!')
+        return email
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy, reverse
+from users.models import User
+from users.forms import UserRegisterForm, UserProfileForm
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UsersListView(ListView):
+    model = User
+    template_name = 'users/user_list.html'
